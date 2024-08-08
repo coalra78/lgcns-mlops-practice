4c9b79f849d2178ba02e8e5a0950e29912bc4ca4
coalra78 <coalra78@naver.com>
2024-08-08 02:22:29 +0000

    Add batch_prediction.py

--- batch_prediction.py
+++ batch_prediction.py
@@ -23,10 +23,11 @@ if __name__ == "__main__":
     DATE = datetime.now().strftime("%Y%m%d")
     test = pd.read_csv(os.path.join(DATA_PATH, "house_rent_test.csv"))
     model = joblib.load(os.path.join(ARTIFACT_PATH, "model.pkl"))
+    logger.debug("Load the test data and the model...")
 
     X = test.drop(["id", "rent"], axis=1, inplace=False)
     id_ = test["id"].to_numpy()
-    y = test["rent"].to_numpy()
+  #  y = test["rent"].to_numpy()
 
     # 테스트 데이터에 대한 피처 데이터 저장
     model["preprocessor"].transform(X=X).to_csv(
@@ -36,7 +37,7 @@ if __name__ == "__main__":
 
     pred_df = pd.DataFrame({"user": id_, "rent": np.expm1(model.predict(X))})
 
-    logger.info(f"Batch prediction for {len(pred_df)} users is created.")
+    logger.info(f"Batch prediction for {len(pred_df)} users is created...")
 
     save_path = os.path.join(PREDICTION_PATH, f"{DATE}_rent_prediction.csv")
     pred_df.to_csv(save_path, index=False)
